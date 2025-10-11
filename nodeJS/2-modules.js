dd305867c7bea19c1249c1990b06fac2be3bf245
Wahid Halim <wahidkeo@gmail.com>
2025-10-11 12:20:09 +0430

    build-in module and os module

--- nodeJS/2-modules.js
+++ nodeJS/2-modules.js
@@ -3,7 +3,22 @@
 
 const greeting = require("./utils");
 const { john, peter } = require("./names");
+require("./mindGrenade");
+// greeting(john);
+// greeting(peter);
+// greeting("wahid");
 
-greeting(john);
-greeting(peter);
-greeting("wahid");
+const os = require('os');
+const { log } = require("console");
+
+const user = os.userInfo();
+const osMethods = {
+    name: os.type(),
+    totalMemory: os.totalmem(),
+    freeMemory: os.freemem(),
+    uptime: os.uptime()
+}
+
+console.log(osMethods)
+
+
