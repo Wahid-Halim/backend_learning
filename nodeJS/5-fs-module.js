d60557daaf26c7a2bd9f0fa233ec88f731e011ba
Wahid Halim <wahidkeo@gmail.com>
2025-10-12 16:26:49 +0430

    fs module (async) writeFile & readFile learn

--- nodeJS/5-fs-module.js
+++ nodeJS/5-fs-module.js
@@ -1,13 +1,33 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync, writeFileSync, readFile, writeFile } = require("fs");
 
 const osModule = readFileSync("./3-os-module.js", "utf8");
 const first = readFileSync("./content/first.txt", "utf8");
 
-console.log(first);
+// console.log(first);
 
+// fs-module sync
 writeFileSync(
   "./content/result-sync.txt",
   `This is the result of writeFileSync method: ${osModule}`
 );
 
 writeFileSync("./content/result-sync.txt", first, { flag: "a" });
+
+// fs (async)
+
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) throw err;
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
+    if (err) throw err;
+    const second = result;
+    writeFile(
+      "./content/result-sync.txt",
+      `${first}:${second}`,
+      (err, result) => {
+        if (err) throw err;
+        console.log("File written successfully");
+      }
+    );
+  });
+});
