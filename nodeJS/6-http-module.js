3727f38bb52e39d453f0f27fa42a0582e7656e92
Wahid Halim <wahidkeo@gmail.com>
2025-10-13 18:00:39 +0430

    http module creating req and res createServer

--- nodeJS/6-http-module.js
+++ nodeJS/6-http-module.js
@@ -1,10 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write("Welcome to my first server");
-  res.end();
+  if (req.url === "/") {
+    res.end("Welcome to Home page");
+    return;
+  }
+  if (req.url === "/about") {
+    res.end("About Page");
+    return;
+  }
+
+  res.end(`
+        <h1>Opss</h1>
+        <p>We can not found the page you looking for </p>
+        <a href="/">back to home</a>
+        `);
 });
 
 server.listen(5000);
-
-
