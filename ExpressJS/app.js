273b09e4ddbed0417692ea6c181dbe7a2213b271
Wahid Halim <wahidkeo@gmail.com>
2025-10-19 11:56:58 +0430

    express get and all method learn

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,37 +1,22 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const { readFileSync } = require("fs");
-
-// get all files
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyles = readFileSync("./navbar-app/styles.css");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  console.log(url);
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  } else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homeStyles);
-    res.end();
-  } else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homeImage);
-    res.end();
-  } else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1 style='background-color: red;'>Page not found</h1>");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.status(200).send("Home Page");
 });
 
-server.listen(5000);
+app.get("/about", (req, res) => {
+  res.status(200).send("About page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Page not found 404");
+});
+
+app.get("/welcome", (req, res) => {
+  res.send("Welcome to this page");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
