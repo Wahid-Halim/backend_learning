e27d7e697ad74ab637d11cefd9f6031d1c34bb88
Wahid Halim <wahidkeo@gmail.com>
2025-10-19 15:54:48 +0430

    express params req.params learn

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -4,7 +4,8 @@ const app = express();
 const { products } = require("./data");
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send(`<h1>Home Page</h1>
+    <a href="/api/products">products</a>`);
 });
 
 app.get("/api/products", (req, res) => {
@@ -17,6 +18,24 @@ app.get("/api/products", (req, res) => {
     };
   });
   res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  //   console.log(req);
+  //   console.log(req.params);
+
+  const { productID } = req.params;
+
+  const singleProduct = products.find((product) => {
+    if (product.id === Number(productID)) {
+      return product;
+    }
+  });
+
+  if (!singleProduct) {
+    return res.send("Product does not found 404");
+  }
+  return res.json(singleProduct);
 });
 
 app.listen(5000, () => {
