68cf97034db82c1677f88e867d93fdb9bd2b32ba
Wahid Halim <wahidkeo@gmail.com>
2025-10-19 10:40:01 +0430

    http module creater server and send html file

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,40 +1,25 @@
 const http = require("http");
-const fs = require("fs");
-const path = require("path");
 
-// Create server
+const { readFileSync } = require("fs");
+
+// get all files
+const homePage = readFileSync("./index.html");
+
 const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Welcome to Wahid's Node.js Server</h1>");
-  } else if (req.url === "/about" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(
-      "<h1>About Page</h1><p>This is a basic server using HTTP module.</p>"
-    );
-  } else if (req.url === "/file" && req.method === "GET") {
-    const filePath = path.join(__dirname, "content", "sample.txt");
-
-    // Use stream to send file
-    const readable = fs.createReadStream(filePath, "utf8");
-    readable.on("open", () => {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      readable.pipe(res); // Pipe file content to response
-    });
-    readable.on("error", (err) => {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("File not found");
-    });
-  }
-  // Route: Not found
-  else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("<h1>404 - Page Not Found</h1>");
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<i>About page</i>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1 style='background-color: red;'>Page not found</h1>");
+    res.end();
   }
 });
 
-// Start server
-const PORT = 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+server.listen(5000);
