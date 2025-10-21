416d870fff53df763459e93c2c8c0a0bedbb90a6
Wahid Halim <wahidkeo@gmail.com>
2025-10-21 11:34:34 +0430

    POST learned with two favor

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -4,9 +4,19 @@ let { people } = require("./data");
 
 app.use(express.static("./methods-public"));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get("./api/people", (req, res) => {
-  res.status(200).json({ success: true, date: people });
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name)
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide a input value" });
+  res.status(201).json({ success: true, person: name });
 });
 
 app.post("/login", (req, res) => {
