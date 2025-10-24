ee5cb2f3e2d8618df66f923c56346f4e8b2fb791
Wahid Halim <wahidkeo@gmail.com>
2025-10-24 17:00:52 +0430

    start creating task api

---
+++ task-manger/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+
+app.get("/hello", (req, res) => {
+  res.send("task manger app!");
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("Server is running on port 5000");
+});
