4437f48968a5f0f07fd8b16dc1415bceb1904310
Wahid Halim <wahidkeo@gmail.com>
2025-11-11 16:11:00 +0430

    redirecting from /message route

--- maxBackend/app.js
+++ maxBackend/app.js
@@ -1,6 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("content-type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Node.js server</title></head>");
+    res.write(
+      "<form action='message' method='POST'><input type='text' name='message' /> <button type='submit'>Submit</button></form>"
+    );
+    return res.write("</html>");
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Dummy message");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
   res.setHeader("content-type", "text/html");
   res.write("<html>");
   res.write("<head><title>Node.js server</title></head>");
