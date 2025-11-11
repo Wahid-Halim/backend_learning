b0bccb3ebf6aa30225b4ea0b358d2f27ff042605
Wahid Halim <wahidkeo@gmail.com>
2025-11-11 15:16:34 +0430

    repeat req and res object

--- maxBackend/app.js
+++ maxBackend/app.js
@@ -1,9 +1,12 @@
 const http = require("http");
 
-const rqListener = (req, res) => {};
-
 const server = http.createServer((req, res) => {
-  console.log(req);
+  res.setHeader("content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Node.js server</title></head>");
+  res.write("<h1>Hello World! </h1>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
